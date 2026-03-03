059f86914cae83e2cd74a94fd6c3530b78e1b300
nate <Jamalnathan7@gmail.com>
2026-03-03 11:17:45 -0500

    Wordguess game successfully picks from a library of coding terminology, next steps are to add a way to loop back around and repeat

---
+++ python/test_wordguess.py
@@ -0,0 +1,14 @@
+"""
+Test class for Hangman implementation.
+
+@author xt0fer
+@version 1.0.0
+@date 5/27/21 11:02 AM
+"""
+
+from wordguess import WordGuess
+
+
+class TestWordGuess:
+    """Test class for WordGuess - ready for test implementation."""
+    pass
--- python/wordguess.py
+++ python/wordguess.py
@@ -5,8 +5,68 @@ WordGuess game implementation.
 @version 1.0.0
 @date 5/27/21 11:02 AM
 """
+#add a way to import more random words focusing on the theme of programming languages, data structures, and algorithms. I will also add a way to keep track of the user's score and allow them to play multiple rounds of the game.
+import random
 import person
 
 class WordGuess:
     """WordGuess game class - ready for implementation."""
+    #Below is where I will create a simple loop to run a WordGuess game. I will need to create a list of words, randomly select one, and then allow the user to guess letters until they either win or lose.
+    def __init__(self):
+        self.words = ["python", "java", "Sqlscript", "javascript", "ruby", "csharp", "cryptography", "algorithm", "data", "structure"]
+        self.selected_word = random.choice(self.words)
+        self.guessed_letters = set()
+        self.max_attempts = 6
+        self.attempts = 0
     pass
+    def play(self):
+         while True:
+            pass
+            guess = input("Welcome to Word Guess! Guess a letter: ").lower()
+            if guess in self.selected_word:
+                self.guessed_letters.add(guess)
+                print(f"Good guess! {guess} is in the word.")
+            elif guess in self.guessed_letters:
+                print(f"You've already guessed {guess}. Try a different letter.")
+                continue
+            else:
+                self.attempts += 1
+                print(f"Wrong guess! {guess} is not in the word. Attempts left: {self.max_attempts - self.attempts}")
+            if self.attempts >= self.max_attempts:
+                print(f"Game over! The word was: {self.selected_word}")
+                break
+
+          # Check if the player has guessed all letters in the word
+            if all(letter in self.guessed_letters for letter in self.selected_word):
+                print(f"Congratulations! You've guessed the word: {self.selected_word}")
+                break
+
+            # Insert an option for te user to guess the entire word if they have guessed at least 3 letters correctly
+            if len(self.guessed_letters) >= 3:
+                full_guess = input("Do you want to guess the entire word? (y/n): ").lower()
+                if full_guess == "y":
+                    word_guess = input("Enter your guess for the word: ").lower()
+                    if word_guess == self.selected_word:
+                        print(f"Congratulations! You've guessed the word: {self.selected_word}")
+                        break
+                    else:
+                        self.attempts += 1
+                        print(f"Wrong guess! {word_guess} is not the word. Attempts left: {self.max_attempts - self.attempts}")
+                        if self.attempts >= self.max_attempts:
+                            print(f"Game over! The word was: {self.selected_word}")
+                            break
+
+            
+
+    
+
+
+
+
+def main():
+    game = WordGuess()
+    game.play()
+
+
+if __name__ == "__main__":
+        main()
